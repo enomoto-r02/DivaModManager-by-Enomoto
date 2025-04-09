0b0a4ca33b5e26a61fc810c82fa541820e03a53a
榎本 瞬 <enomoto.r02@outlook.jp>
2025-04-09 15:24:54 +0900

    ・変更の戻し

--- DivaModManager/UI/MainWindow.xaml.cs
+++ DivaModManager/UI/MainWindow.xaml.cs
@@ -21,7 +21,6 @@ using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using Tomlyn;
 using Tomlyn.Model;
